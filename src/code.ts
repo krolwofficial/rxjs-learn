359304d72444d04cf23992a344f0d7f6f214f531
Wiktor Król <krolwofficial@gmail.com>
2019-09-10 13:29:17 +0200

    streams & observables

--- src/code.ts
+++ src/code.ts
@@ -1,3 +1,31 @@
 import { Observable } from "rxjs";
 
-console.log(Observable);
+let observable = Observable.create((observer: any) => {
+  try {
+    observer.next("How are you");
+    setInterval(() => {
+      observer.next("I am good");
+    }, 200);
+  } catch (err) {
+    observer.error(err);
+  }
+});
+
+let observer = observable.subscribe(
+  (x: any) => addItem(x),
+  (error: any) => addItem(error),
+  () => addItem("Completed")
+);
+
+let observer2 = observable.subscribe((x: any) => addItem(x));
+
+observer.add(observer2);
+
+setTimeout(() => observer.unsubscribe(), 601);
+
+function addItem(val: any) {
+  var node = document.createElement("li");
+  var textnode = document.createTextNode(val);
+  node.appendChild(textnode);
+  document.getElementById("output").appendChild(node);
+}
